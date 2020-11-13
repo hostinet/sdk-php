b37573a4c1174278cbafd909958e0dede01843c9
Hostinet <desarrollo@hostinet.com>
2020-11-13 13:23:12 +0100

    Update domain-dyndns.php

--- examples/domain-dyndns.php
+++ examples/domain-dyndns.php
@@ -11,10 +11,28 @@ define('API_SECRET', '');
 
 $api = new HostinetApi(API_KEY,API_SECRET);
 
+// Set a IP to "www.mydomain.com"
 $response = $api->post("domain/dyndns", [
   'ip' => 'xxx.xxx.xxx.xxx',
   'domain' => 'mydomain.com',
   'host' => 'www',
+]);
+
+print_r($response);
+
+// Set a IP to "mydomain.com"
+$response = $api->post("domain/dyndns", [
+  'ip' => 'xxx.xxx.xxx.xxx',
+  'domain' => 'mydomain.com',
+  'host' => '@',
+]);
+
+print_r($response);
+
+// Set automatic IP to "mydomain.com"
+$response = $api->post("domain/dyndns", [
+  'domain' => 'mydomain.com',
+  'host' => '@',
 ]);
 
 print_r($response);
